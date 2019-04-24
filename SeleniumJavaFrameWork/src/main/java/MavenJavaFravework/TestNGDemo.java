a4627467e061d5384adf5d2e9e9ee9f2a7c6e233
elvin <elvin@192.168.1.4>
2019-04-24 09:16:52 -0400

    First Commit - Added project

---
+++ SeleniumJavaFrameWork/src/main/java/MavenJavaFravework/TestNGDemo.java
@@ -0,0 +1,48 @@
+package MavenJavaFravework;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class TestNGDemo {
+
+
+	 static WebDriver driver = null;
+	
+	@BeforeTest
+	public void setup () {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/elvin/Desktop/Selenium/QA/Selenium/jars/chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	
+	@Test
+	public static void googleSearch() throws InterruptedException {
+		
+		driver.get("http://google.com");
+		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS) ;
+		driver.manage().window().maximize();	
+		
+		WebElement textBox = driver.findElement(By.name("q"));
+		textBox.sendKeys("Automation Step by Step in Selenium");
+		
+		Thread.sleep(5000);
+		
+		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+	}
+	
+	@AfterTest
+	public void Closebrowser() {
+		
+		driver.close();
+		System.out.println("The Process is Successfull");
+	}
+}
+
+
---
+++ SeleniumJavaFrameWork/src/main/java/SeleniumJavaFrameWork/SeleniumJavaFrameWork/App.java
@@ -0,0 +1,13 @@
+package SeleniumJavaFrameWork.SeleniumJavaFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
